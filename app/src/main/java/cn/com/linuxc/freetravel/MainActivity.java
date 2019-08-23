136da56057ad7555bc5ae743bfef67634f1265a1
skawu <maoxudong0813@163.com>
2019-08-23 21:05:44 +0800

    增加登录界面和注册界面，未设计，只实现跳转功能

--- app/src/main/java/cn/com/linuxc/freetravel/MainActivity.java
+++ app/src/main/java/cn/com/linuxc/freetravel/MainActivity.java
@@ -2,13 +2,44 @@ package cn.com.linuxc.freetravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt_sign_in;
+    private Button bt_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 登录界面跳转
+        bt_sign_in = (Button)findViewById(R.id.bt_signin);
+        bt_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(cn.com.linuxc.freetravel.MainActivity.this, MasterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 注册界面跳转
+        bt_sign_up = (Button)findViewById(R.id.bt_signup);
+        bt_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(cn.com.linuxc.freetravel.MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/cn/com/linuxc/freetravel/MasterActivity.java
@@ -0,0 +1,14 @@
+package cn.com.linuxc.freetravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+    }
+}
